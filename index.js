9d4c9f1a4948c0fa5f8b8fae9fbc62df5097be69
julianalondono <juli1503londono@gmail.com>
2019-08-17 09:22:51 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var casa = [];
 var jug1 = [];
+var victorioso=0;
+
 //funcion que genera 4 numeros aleatorios y los imprime en los parrafos demo1, 2, 3 y 4
 function myFunction() {
   //Ciclo para generar los 4 números aleatorios
@@ -15,39 +17,54 @@ function myFunction() {
     }
   }
 
+  
   document.getElementById('casa').style.display = 'inline-block';
   document.getElementById('jugador').style.display = 'block';
 }
+//Verifica que ningún jugador se pase del conteo de 21 puntos
+function verificarGanador() {
+  var contJug=contarCartas("jugador");
+  var contCasa=contarCartas("casa");
+  if (contJug>21||contCasa>21) {
+    victorioso=1;
+  }
+}
 //Esta función genera una carta adicional al jugador
 function cartaAdcional() {
-  //Contamos las cartas actuales del jugador y la casa
-  var contadorCasa = contarCartas("casa");
-  var contadorJug = contarCartas("jugador");
+  //Antes de adicionar carta se verifica que no exista ganador
+  verificarGanador();
+  if(victorioso!=0){
+    alert("Ya hay perdedor");
+  }else{
+      //Contamos las cartas actuales del jugador y la casa
+      var contadorCasa = contarCartas("casa");
+      var contadorJug = contarCartas("jugador");
+      // Si el contador de la casa es menor o igual a 16, se añade una nueva carta
+      if (contadorCasa <= 16) {
+        // Se crea un nuevo Span con id "casa#" donde '#' es el número siguiente de tags casa
+        let newSpan = document.createElement("SPAN");
+        newSpan.setAttribute("id", `casa${casa.length}`);
 
-  // Si el contador de la casa es menor o igual a 16, se añade una nueva carta
-  if (contadorCasa <= 16) {
-    // Se crea un nuevo Span con id "casa#" donde '#' es el número siguiente de tags casa
-    let newSpan = document.createElement("SPAN");
-    newSpan.setAttribute("id", `casa${casa.length}`);
-    // Se crea una nueva carta y se pushea al arreglo de casa
-    casa.push(newCarta(Math.floor(Math.random() * 52 + 1)));
-    // Se le añade el número de la carta al nuevo Span
-    newSpan.innerHTML = " " + casa[casa.length - 1].numero;
+        // Se crea una nueva carta y se pushea al arreglo de casa
+        casa.push(newCarta(Math.floor(Math.random() * 52 + 1)));
+        // Se le añade el número de la carta al nuevo Span
+        newSpan.innerHTML = " " + casa[casa.length - 1].numero;
 
-    // Se le añade como hijo el nuevo Span al div de id 'casa'
-    document.getElementById("casa").appendChild(newSpan);
+        // Se le añade como hijo el nuevo Span al div de id 'casa'
+        document.getElementById("casa").appendChild(newSpan);
+      }
+      //Se revisa si la casa perdio
+      if (contadorCasa > 21) alert("La Casa pierde");
+
+      //Se le agrega una carta al usuario (en el arreglo y en el html).
+      jug1.push(newCarta(Math.floor(Math.random() * 52 + 1)));
+      $("#jugador").append("<span id='jugador" + jug1.length + "'></span>");
+      $("#jugador" + jug1.length).html(jug1[jug1.length - 1].numero + " ");
+
+      //Se revisa si el usuario perdio
+      if (contadorJug > 21) alert("Jugador pierde!");
+    }
   }
-  //Se revisa si la casa perdio
-  if (contadorCasa > 21) alert("La Casa pierde");
-
-  //Se le agrega una carta al usuario (en el arreglo y en el html).
-  jug1.push(newCarta(Math.floor(Math.random() * 52 + 1)));
-  $("#jugador").append("<span id='jugador" + jug1.length + "'></span>");
-  $("#jugador" + jug1.length).html(jug1[jug1.length - 1].numero + " ");
-
-  //Se revisa si el usuario perdio
-  if (contadorJug > 21) alert("Te pasaste!");
-}
 
 //Retorna la suma de las cartas
 function contarCartas(jugador) {
@@ -68,11 +85,11 @@ function contarCartas(jugador) {
   return contador;
 }
 
-/* 
+/*
     newCarta(carta)
     función que recibe un int
     función que retorna un objeto con atributos (str)pinta, (int)numero
-        pinta es "diamante", "corazon", "trebol", "pica", numero va de 1 a 13
+        pinta es "diamante", "corazon", "trevol", "pica", numero va de 1 a 13
 */
 function newCarta(carta) {
   // el rango del numero tiene que estar entre 1 y 52
@@ -81,7 +98,7 @@ function newCarta(carta) {
     return -1;
   }
 
-  pintas = ["diamante", "corazon", "trebol", "pica"];
+  pintas = ["diamante", "corazon", "trevol", "pica"];
   // se obtiene una posición del array de pintas
   var posPinta = Math.floor((carta - 1) / 13);
   var pinta = pintas[posPinta];
@@ -92,7 +109,3 @@ function newCarta(carta) {
     numero: numero + 1
   };
 }
-
-
-
-
